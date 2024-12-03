5a5dac813b0d6f3c1e450fa1e4479dc60fac14f8
Jakub Vlasák <vlasakjakub@gmail.com>
2024-12-03 17:24:55 +0100

    Day03 second part solution

--- day03/solution03.go
+++ day03/solution03.go
@@ -7,23 +7,80 @@ import (
 )
 
 func Main() {
-	fmt.Println("Result of the expression is: ", computeAllMul(input01))
+	fmt.Println("Result for the first solution is: ", firstSolution(input01))
+	fmt.Println("Result for the second solution is: ", secondSolution(input01))
 }
 
-func computeAllMul(input string) int {
-	reg, _ := regexp.Compile(`mul\(\d+,\d+\)`)
-	regForNums, _ := regexp.Compile(`\d+`)
+func secondSolution(input string) int {
+	mulReg, _ := regexp.Compile(`mul\(\d+,\d+\)`)
+	doReg, _ := regexp.Compile(`do\(\)`)
+	dontReg, _ := regexp.Compile(`don't\(\)`)
+	doIndex, dontIndex, expIndex := 0, 0, 0
 
-	match := reg.FindAllString(input, -1)
+	// Get indexes for all the expressions
+	doIndexes := parseIndexes(doReg.FindAllStringSubmatchIndex(input, -1))
+	dontIndexes := parseIndexes(dontReg.FindAllStringSubmatchIndex(input, -1))
+	expIndexes := parseIndexes(mulReg.FindAllStringSubmatchIndex(input, -1))
+
+	// Get all mul the expressions
+	expressions := mulReg.FindAllString(input, -1)
+
 	solution := 0
+	canDo := true
 
-	for _, expression := range match {
-		nums := regForNums.FindAllString(expression, -1)
-		firstNum, _ := strconv.Atoi(nums[0])
-		secondNum, _ := strconv.Atoi(nums[1])
-
-		solution += firstNum * secondNum
+	for i := range len(input) {
+		if i == doIndexes[doIndex] {
+			if doIndex < len(doIndexes)-1 {
+				doIndex++
+			}
+			canDo = true
+		}
+		if i == dontIndexes[dontIndex] {
+			if dontIndex < len(dontIndexes)-1 {
+				dontIndex++
+			}
+			canDo = false
+		}
+		if expIndex < len(expIndexes) && i == expIndexes[expIndex] {
+			if canDo {
+				solution += computeMulFromExpression(expressions[expIndex])
+			}
+			expIndex++
+		}
 	}
 
 	return solution
+}
+
+func firstSolution(input string) int {
+	reg, _ := regexp.Compile(`mul\(\d+,\d+\)`)
+	match := reg.FindAllString(input, -1)
+
+	solution := 0
+	for _, expression := range match {
+		solution += computeMulFromExpression(expression)
+	}
+
+	return solution
+}
+
+func computeMulFromExpression(expression string) int {
+	regForNums, _ := regexp.Compile(`\d+`)
+	nums := regForNums.FindAllString(expression, -1)
+	return computeMul(nums)
+}
+
+func computeMul(nums []string) int {
+	firstNum, _ := strconv.Atoi(nums[0])
+	secondNum, _ := strconv.Atoi(nums[1])
+
+	return firstNum * secondNum
+}
+
+func parseIndexes(input [][]int) []int {
+	indexes := make([]int, len(input))
+	for i, values := range input {
+		indexes[i] = values[0]
+	}
+	return indexes
 }
--- day03/solution03_test.go
+++ day03/solution03_test.go
@@ -2,11 +2,18 @@ package day03
 
 import "testing"
 
-var testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+var testInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 func TestComputeAllMul(t *testing.T) {
-	solution := computeAllMul(testInput)
+	solution := firstSolution(testInput)
 	if solution != 161 {
 		t.Fatalf("Expected solution to be 161, got %d", solution)
+	}
+}
+
+func TestSecondSolution(t *testing.T) {
+	solution := secondSolution(testInput)
+	if solution != 48 {
+		t.Fatalf("Expected solution to be 48, got %d", solution)
 	}
 }
